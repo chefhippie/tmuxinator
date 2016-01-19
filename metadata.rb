83a20c49bf92ae3f3df0d084d7b21a829d10c9e8
Thomas Boerger <tboerger@suse.de>
2016-01-19 10:31:16 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name "tmuxinator"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures tmuxinator"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+depends "ruby"
+depends "tmux"
+recipe "tmuxinator", "Installs/Configures tmuxinator"
+
+supports "suse", ">= 12.3"
+supports "debian", ">= 7.0"
+supports "ubuntu", ">= 12.04"
